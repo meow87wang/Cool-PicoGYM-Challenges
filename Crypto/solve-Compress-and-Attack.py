eebf442746cd6ee2d36cef081b07f83732df5657
meow87wang <1999119wang@gmail.com>
2024-04-18 10:23:33 +0800

    Add Compress and Attack code

---
+++ Crypto/solve-Compress-and-Attack.py
@@ -0,0 +1,56 @@
+import zlib
+import string
+from itertools import product
+from collections import defaultdict
+import pickle
+from telnetlib import Telnet
+from tqdm import tqdm
+
+def compress(text):
+    return zlib.compress(bytes(text.encode("utf-8")))
+
+def resvLength(tn, text):
+    tn.write(text + b"\n")
+    tn.read_until(b"\n")
+    tn.read_until(b"\n")
+    x = int(tn.read_until(b"\n").decode())
+    return x
+
+
+
+flag = "flag{bcde}"
+
+print("="*20 + "Test" + "="*20)
+
+print(len(compress(flag + "flag{x")))
+print(len(compress(flag + "flag{y")))
+print(len(compress(flag + "flag{b")))
+
+print()
+
+print(len(compress(flag + "flag{bx")))
+print(len(compress(flag + "flag{by")))
+print(len(compress(flag + "flag{bc")))
+
+print("="*40)
+
+charset = string.ascii_lowercase + string.ascii_uppercase +"{}_"
+lengths = defaultdict(list)
+flag = "picoCTF{"
+
+for i in range(20):
+    tn = Telnet('mercury.picoctf.net', 57393)
+    minlength = 100000
+    res = '*'
+    for j in tqdm(charset):
+        length = resvLength(tn,(flag + j).encode())
+        if length < minlength:
+            minlength = length
+            res = j
+    flag += res
+    print(flag)
+    
+tn.close()
+
+
+# Flag: picoCTF{sheriff_you_solved_the_crime}
